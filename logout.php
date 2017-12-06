46c4a3a987fa23b55575b29171d98a614de2fff3
Lukaka4331 <105021044@live.asia.edu.tw>
2017-12-06 11:39:25 +0800

    11111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111

---
+++ logout.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: USER
+ * Date: 2017/12/6
+ * Time: 上午 10:53
+ */
+?>
+<?php session_start(); ?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
+//將session清空
+unset($_SESSION['username']);
+echo '登出中......';
+echo '<meta http-equiv=REFRESH CONTENT=1;url=index.php>';
+?>
